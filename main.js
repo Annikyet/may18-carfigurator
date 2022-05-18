cc2477c0f584667e7ffecffe8c83235c4a20bc18
Annika <annikyet@icloud.com>
2022-05-18 15:36:35 -0600

    ugh... fell down a Class rabbit hole. Starting over the dirty way

---
+++ main.js
@@ -0,0 +1,65 @@
+const options = {
+  bodies: {
+    name: 'Body',
+    liftback: {
+      name: 'Liftback',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 0
+    },
+    sedan: {
+      name: 'Sedan',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 3000
+    },
+    convertible: {
+      name: 'Convertible',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 6000
+    }
+  },
+  engines: {
+    name: 'Engine',
+    rwd: {
+      name: '240HP RWD Electric',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 0
+    },
+    phev: {
+      name: '320HP RWD Plug-in Hybrid',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 6000
+    },
+    awd: {
+      name: '360HP Electric',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 16000
+    }
+  },
+  colors: {
+    name: 'Color',
+    red: {
+      name: 'Burgundy',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 0
+    },
+    blue: {
+      name: 'Teal',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 0
+    },
+    black: {
+      name: 'Onyx',
+      description: 'Meow meow meow meow meow meow meow meow meow.',
+      image: '//thiscatdoesnotexist.com',
+      price: 1000
+    }
+  }
+}
